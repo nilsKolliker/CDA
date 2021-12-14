dba52ddd48355ca57561f538448423eeed1375f6
nilsKolliker <nils.kolliker@gmail.com>
2021-12-14 12:43:29 +0100

    connection par json

--- Acces aux donnees/getion de produit/getion de produit/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Acces aux donnees/getion de produit/getion de produit/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace getion_de_produit {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/getion de produit;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/getion de produit;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
