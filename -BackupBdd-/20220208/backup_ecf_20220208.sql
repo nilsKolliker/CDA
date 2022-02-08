fb32e9ea51b5d73f6582dd6eeecb938fe4737817
59011-07-07 <nils.kolliker@gmail.com>
2022-02-08 12:28:09 +0100

    08/02/2022 12:28:07,59

---
+++ -BackupBdd-/20220208/backup_ecf_20220208.sql
@@ -0,0 +1,90 @@
+-- MySQL dump 10.13  Distrib 5.7.31, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ecf
+-- ------------------------------------------------------
+-- Server version	5.7.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `ecf`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `ecf` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ecf`;
+
+--
+-- Table structure for table `groupes`
+--
+
+DROP TABLE IF EXISTS `groupes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groupes` (
+  `IdGroupe` int(11) NOT NULL AUTO_INCREMENT,
+  `NomDuGroupe` varchar(50) NOT NULL,
+  `NombreDeFollowers` int(11) NOT NULL,
+  `Logo` varchar(250) NOT NULL,
+  PRIMARY KEY (`IdGroupe`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groupes`
+--
+
+LOCK TABLES `groupes` WRITE;
+/*!40000 ALTER TABLE `groupes` DISABLE KEYS */;
+INSERT INTO `groupes` VALUES (1,'TheGroup',3,'logo1.jpg'),(2,'Meilleur Groupe',30,'logo2.png');
+/*!40000 ALTER TABLE `groupes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `musiciens`
+--
+
+DROP TABLE IF EXISTS `musiciens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `musiciens` (
+  `idMusicien` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(50) NOT NULL,
+  `Prenom` varchar(50) NOT NULL,
+  `Instrument` varchar(50) NOT NULL,
+  `IdGroupe` int(11) NOT NULL,
+  PRIMARY KEY (`idMusicien`),
+  KEY `FK_Membres_Groupes` (`IdGroupe`),
+  CONSTRAINT `FK_Membres_Groupes` FOREIGN KEY (`IdGroupe`) REFERENCES `groupes` (`IdGroupe`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `musiciens`
+--
+
+LOCK TABLES `musiciens` WRITE;
+/*!40000 ALTER TABLE `musiciens` DISABLE KEYS */;
+INSERT INTO `musiciens` VALUES (1,'Dupond','Toto','guitare',1),(4,'Durand','Titi','batterie',2),(5,'alan','alan','guitard',2),(6,'alan','eoe','epeee',2),(7,'alan','oko','guitard',2);
+/*!40000 ALTER TABLE `musiciens` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-08 12:26:05
